1934ecce13d75feaa4c61e19d9615bbbe2514e2a
Jose Manuel <manuelvaldez8642@gmail.com>
2019-05-22 22:28:46 -0500

    First functional commit

---
+++ lectorTxt/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace lectorTxt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.lastname = new System.Windows.Forms.TextBox();
+            this.inputMessage = new System.Windows.Forms.RichTextBox();
+            this.execute = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellido";
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(67, 125);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(135, 26);
+            this.name.TabIndex = 2;
+            // 
+            // lastname
+            // 
+            this.lastname.Location = new System.Drawing.Point(66, 204);
+            this.lastname.Name = "lastname";
+            this.lastname.Size = new System.Drawing.Size(136, 26);
+            this.lastname.TabIndex = 3;
+            // 
+            // inputMessage
+            // 
+            this.inputMessage.Location = new System.Drawing.Point(254, 42);
+            this.inputMessage.Name = "inputMessage";
+            this.inputMessage.Size = new System.Drawing.Size(552, 352);
+            this.inputMessage.TabIndex = 4;
+            this.inputMessage.Text = "";
+            // 
+            // execute
+            // 
+            this.execute.Location = new System.Drawing.Point(26, 288);
+            this.execute.Name = "execute";
+            this.execute.Size = new System.Drawing.Size(75, 29);
+            this.execute.TabIndex = 5;
+            this.execute.Text = "Ejecutar";
+            this.execute.UseVisualStyleBackColor = true;
+            this.execute.Click += new System.EventHandler(this.execute_Click);
+            // 
+            // save
+            // 
+            this.save.BackColor = System.Drawing.Color.Transparent;
+            this.save.Location = new System.Drawing.Point(151, 288);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(81, 29);
+            this.save.TabIndex = 6;
+            this.save.Text = "Guardar";
+            this.save.UseVisualStyleBackColor = false;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(863, 33);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(84, 29);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(135, 30);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 450);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.execute);
+            this.Controls.Add(this.inputMessage);
+            this.Controls.Add(this.lastname);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Lector txt";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox lastname;
+        private System.Windows.Forms.RichTextBox inputMessage;
+        private System.Windows.Forms.Button execute;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+    }
+}
+
---
+++ lectorTxt/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+// Autor: José Manuel Valdez González
+namespace lectorTxt
+{
+    public partial class Form1 : Form
+    {
+        string ruta;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            StreamWriter escribir = new StreamWriter(ruta, true);
+            try
+            {
+                escribir.WriteLine("Nombre: "+ name.Text);
+                escribir.WriteLine("Apellido: " + lastname.Text);
+                escribir.WriteLine("\n");
+                name.Text = "";
+                lastname.Text = "";
+            }
+            catch
+            {
+                MessageBox.Show("error");
+            }
+            escribir.Close();
+        }
+
+        private void execute_Click(object sender, EventArgs e)
+        {
+            StreamReader leer = new StreamReader(ruta);
+            string linea;
+            inputMessage.Clear();
+            try
+            {
+                linea = leer.ReadLine();
+                while(linea != null){
+                    inputMessage.AppendText(linea+"\n");
+                    linea = leer.ReadLine();
+                }
+            }
+            catch{
+                MessageBox.Show("error");
+            }
+            leer.Close();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Archivos txt(*.txt)|*.txt";
+            open.Title = "Archivos txt";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                ruta = open.FileName;
+                inputMessage.AppendText(ruta);
+            }
+            open.Dispose();
+        }
+    }
+}
