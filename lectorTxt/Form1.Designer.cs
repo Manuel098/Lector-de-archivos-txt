7f5ba15cc541a5f46b212fa7e029762cb8d64afc
Jose Manuel <manuelvaldez8642@gmail.com>
2019-05-24 14:03:20 -0500

    second commit

--- lectorTxt/Form1.Designer.cs
+++ lectorTxt/Form1.Designer.cs
@@ -29,9 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.name = new System.Windows.Forms.TextBox();
-            this.lastname = new System.Windows.Forms.TextBox();
+            this.text = new System.Windows.Forms.TextBox();
             this.inputMessage = new System.Windows.Forms.RichTextBox();
             this.execute = new System.Windows.Forms.Button();
             this.save = new System.Windows.Forms.Button();
@@ -44,34 +42,18 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 92);
+            this.label1.Location = new System.Drawing.Point(100, 92);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.Size = new System.Drawing.Size(48, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Nombre";
+            this.label1.Text = "Texto";
             // 
-            // label2
+            // text
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 171);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 20);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Apellido";
-            // 
-            // name
-            // 
-            this.name.Location = new System.Drawing.Point(67, 125);
-            this.name.Name = "name";
-            this.name.Size = new System.Drawing.Size(135, 26);
-            this.name.TabIndex = 2;
-            // 
-            // lastname
-            // 
-            this.lastname.Location = new System.Drawing.Point(66, 204);
-            this.lastname.Name = "lastname";
-            this.lastname.Size = new System.Drawing.Size(136, 26);
-            this.lastname.TabIndex = 3;
+            this.text.Location = new System.Drawing.Point(67, 125);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(135, 26);
+            this.text.TabIndex = 2;
             // 
             // inputMessage
             // 
@@ -136,9 +118,7 @@
             this.Controls.Add(this.save);
             this.Controls.Add(this.execute);
             this.Controls.Add(this.inputMessage);
-            this.Controls.Add(this.lastname);
-            this.Controls.Add(this.name);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.text);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -154,9 +134,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox name;
-        private System.Windows.Forms.TextBox lastname;
+        private System.Windows.Forms.TextBox text;
         private System.Windows.Forms.RichTextBox inputMessage;
         private System.Windows.Forms.Button execute;
         private System.Windows.Forms.Button save;
--- lectorTxt/Form1.cs
+++ lectorTxt/Form1.cs
@@ -11,6 +11,7 @@ using System.IO;
 
 
 // Autor: José Manuel Valdez González
+
 namespace lectorTxt
 {
     public partial class Form1 : Form
@@ -26,17 +27,15 @@ namespace lectorTxt
             StreamWriter escribir = new StreamWriter(ruta, true);
             try
             {
-                escribir.WriteLine("Nombre: "+ name.Text);
-                escribir.WriteLine("Apellido: " + lastname.Text);
-                escribir.WriteLine("\n");
-                name.Text = "";
-                lastname.Text = "";
+                escribir.WriteLine(text.Text);
+                escribir.Close();
+                rule();
+                text.Text = "";
             }
             catch
             {
                 MessageBox.Show("error");
             }
-            escribir.Close();
         }
 
         private void execute_Click(object sender, EventArgs e)
@@ -69,6 +68,83 @@ namespace lectorTxt
                 inputMessage.AppendText(ruta);
             }
             open.Dispose();
+        }
+
+        private void rule()
+        {
+            StreamWriter escribir = new StreamWriter(ruta, true);
+            bool complete = true, valid = true, temp = false;
+            string estados = "initial";
+            foreach (char caracter in text.Text)
+            {
+                int i;
+                if (int.TryParse(caracter.ToString(), out i))
+                {
+                    switch (estados)
+                    {
+                        case "initial":
+                            escribir.WriteLine("0d: " + caracter);
+                            estados = "Efinal1";
+                            break;
+                        case "Efinal1":
+                            escribir.WriteLine("1d: " + caracter);
+                            break;
+                        case "e2":
+                            escribir.WriteLine("2d: " + caracter);
+                            temp = false;
+                            estados = "Efinal2";
+                            break;
+                        case "Efinal2":
+                            escribir.WriteLine("3d: " + caracter);
+                            break;
+                    }
+                }
+                else
+                {
+                    if (valid)
+                    {
+                        switch (caracter)
+                        {
+                            case '.':
+                                escribir.WriteLine("1 . : " + caracter);
+                                estados = "e2";
+                                temp = true;
+                                valid = false;
+                                break;
+                            case 'e':
+                                escribir.WriteLine("1E: " + caracter);
+                                valid = false;
+                                temp = true;
+                                estados = "e2";
+                                break;
+                            case 'E':
+                                escribir.WriteLine("1E: " + caracter);
+                                valid = false;
+                                temp = true;
+                                estados = "e2";
+                                break;
+                            default:
+                                escribir.WriteLine("Input incorrecto.");
+                                complete = false;
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        escribir.WriteLine("Input incorrecto.");
+                        complete = false;
+                    }
+                }
+            }
+            if (complete && temp == false)
+            {
+                escribir.WriteLine("Completado :D");
+            }
+            else
+            {
+                escribir.WriteLine("FALLO :c");
+            }
+            escribir.Close();
         }
     }
 }
